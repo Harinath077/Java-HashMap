afe2552ff8b09cb525524128943e82724f67fe5f
Harinath077 <harinathcreationz@gmail.com>
2026-03-10 17:39:06 +0530

    added : frequency of String problem

--- src/main/java/questions/FrequencyOfString.java
+++ src/main/java/questions/FrequencyOfString.java
@@ -1,4 +1,25 @@
 package questions;
-
+import java.util.HashMap;
 public class FrequencyOfString {
+    private void frequencyOfString(String s){
+        // HashMap to store character and its count
+        HashMap<Character, Integer> frequencyMap  = new HashMap<>();
+
+        // Convert string to character array and iterate through each character
+        for(char ch : s.toCharArray()) {
+
+            // getOrDefault(ch,0) returns 0 if character is not present
+            // otherwise returns existing count
+            frequencyMap .put(ch, frequencyMap .getOrDefault(ch, 0) + 1);
+
+        }
+
+        System.out.println(frequencyMap );
+    }
+
+    public static void main( String[] args){
+        String s = "aabccd";
+        FrequencyOfString obj = new FrequencyOfString();
+        obj.frequencyOfString(s);
+    }
 }
